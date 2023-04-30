a6872cc8b5a505efd21d20d8f8ddfb6c5836f902
Bartek <six@op.pl>
2023-04-30 19:08:05 +0200

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "six_test"
 
     workspaces {
       name = "learn-terraform-github-actions"
